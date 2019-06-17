e5aebe39a15ed341675eeaa3e060566f21ffcc75
Foster Haines <fhaines@rhodesgroup.com>
2019-06-16 22:46:04 -0600

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+console.log('Hello TypeScript!');
+
+let a: number = 234;
+console.log(a);
+
+let b: string = 'Hello';
+console.log(b);
+
+console.log(a);
+
+function squareOf(n: number) {
+    return n * n;
+}
+
+console.log(squareOf(2));
+
+let c = ['test'];
+console.log (c);
+
+let z: number = 1042;
+let y: string = 'apples and oranges';
+const x: string = 'pineapples';
+let w: boolean[] = [true, true, false];
+let v: { type: string } = {type: 'ficus'};
+let u: (number | boolean)[] = [1, false];
+const t: number[] = [3];
+let h: any = null;
+
+
+
